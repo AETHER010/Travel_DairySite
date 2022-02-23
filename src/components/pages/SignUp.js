82f5d802d07a202346bf52bf22feee461d71e0a5
HI MESH <heem.ace@gmail.com>
2022-02-23 12:10:41 +0545

    first commit

---
+++ src/components/pages/SignUp.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import '../../App.css';
+
+export default function SignUp() {
+  return(
+    <div className='sign-up'>
+          <form class="row g-3">
+        <div class="col-md-6">
+          <label for="inputEmail4" class="form-label">Email</label>
+          <input type="email" class="form-control" id="inputEmail4" />
+        </div>
+        <div class="col-md-6">
+          <label for="inputPassword4" class="form-label">Password</label>
+          <input type="password" class="form-control" id="inputPassword4" />
+        </div>
+        <div class="col-12">
+          <label for="inputAddress" class="form-label">Address</label>
+          <input type="text" class="form-control" id="inputAddress"/>
+        </div>
+        
+        <div class="col-md-6">
+          <label for="inputCity" class="form-label">City</label>
+          <input type="text" class="form-control" id="inputCity" />
+        </div>
+        <div class="col-md-4">
+          <label for="inputState" class="form-label">State</label>
+          <select id="inputState" class="form-select">
+            <option selected>Choose...</option>
+            <option>...</option>
+          </select>
+        </div>
+        <div class="col-md-2">
+          <label for="inputZip" class="form-label">Zip</label>
+          <input type="text" class="form-control" id="inputZip" />
+        </div>
+        <div class="col-12">
+          <button type="submit" class="btn btn-primary">Sign up</button>
+        </div>
+      </form>
+      </div>
+  );
+}
